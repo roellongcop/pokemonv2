13f8e0469518689f311c414a5d0d655a625e48bf
roellongcop <longcoproel@gmail.com>
2023-12-27 21:46:54 +0800

    pokemon v2 async

--- App.tsx
+++ App.tsx
@@ -1,10 +1,4 @@
-import {
-  ActivityIndicator, Button,
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  View
-} from 'react-native';
+import {ActivityIndicator, Button, FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
 import {get, useFetch} from "./hooks";
 import Pokemon from "./Pokemon";
 import {BaseType, PokemonList} from "./types";
@@ -14,15 +8,8 @@ export default function App() {
   const {data} = useFetch<PokemonList>('pokemon');
   const [loadingMore, setLoadingMore] = useState<boolean>(false);
   const [pokemonList, setPokemonList] = useState<PokemonList | null>(null);
-
-  useEffect(() => {
-    if (!data) return;
-    setPokemonList(data);
-  }, [data]);
-
   const renderFooter = useCallback(() => {
     if (!pokemonList?.next) return;
-
     return (
       <View>
         <Button
@@ -33,15 +20,19 @@ export default function App() {
       </View>
     );
   }, [loadingMore, pokemonList?.next]);
-
   const renderItem = ({item, index}: {item: BaseType, index: number}) => {
     return <Pokemon name={item.name} key={index.toString()} />;
   };
 
-  async function loadMore() {
-    setLoadingMore(true);
-    const {count, next, previous, results} = await get<PokemonList>(pokemonList?.next);
+  useEffect(() => {
+    if (!data) return;
+    setPokemonList(data);
+  }, [data]);
 
+  async function loadMore() {
+    if (!pokemonList?.next) return;
+    setLoadingMore(true);
+    const {count, next, previous, results} = await get<PokemonList>(pokemonList.next);
     setPokemonList((prev: PokemonList) => {
       const newData: BaseType[] = prev ? [...prev.results, ...results]: results;
       return {
@@ -51,7 +42,6 @@ export default function App() {
         results: newData
       }
     });
-
     setLoadingMore(false);
   }
 
